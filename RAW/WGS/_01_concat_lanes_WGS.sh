94a17a1221beeb0a089d4d4985bb3ebaa33a22a1
Sandra Sánchez <sandrasapi02@gmail.com>
2026-03-24 13:33:06 +0100

    Add script to concatenate WGS fastq files
    This script concatenates WGS fastq files for multiple samples into designated output files.

---
+++ RAW/WGS/_01_concat_lanes_WGS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to concatenate WGS fastq files
+cat *_222UDI-idt-UMI_1.fastq.gz > ND0013_R1.fastq.gz
+cat *_222UDI-idt-UMI_2.fastq.gz > ND0013_R2.fastq.gz
+cat *_271UDI-idt-UMI_1.fastq.gz > ND0323_R1.fastq.gz
+cat *_271UDI-idt-UMI_2.fastq.gz > ND0323_R2.fastq.gz
+cat *_285UDI-idt-UMI_1.fastq.gz > ND0463_R1.fastq.gz
+cat *_285UDI-idt-UMI_2.fastq.gz > ND0463_R2.fastq.gz
+cat *_248UDI-idt-UMI_1.fastq.gz > ND0693_R1.fastq.gz
+cat *_248UDI-idt-UMI_2.fastq.gz > ND0693_R2.fastq.gz
