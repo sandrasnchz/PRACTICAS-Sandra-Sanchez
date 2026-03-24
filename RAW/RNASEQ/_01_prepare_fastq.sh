88285886197ee0e4846d2e685dd1f4d787ed708e
Sandra Sánchez <sandrasapi02@gmail.com>
2026-03-24 13:30:42 +0100

    Add script to prepare FASTQ files from raw data

---
+++ RAW/RNASEQ/_01_prepare_fastq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat HGG2CBBXY_6_1UDI-idt-UMI_1.fastq.gz  > ND0013_R1.fastq.gz
+cat HGG2CBBXY_6_1UDI-idt-UMI_2.fastq.gz  > ND0013_R2.fastq.gz
+cat HGG2CBBXY_7_39UDI-idt-UMI_1.fastq.gz > ND0323_R1.fastq.gz
+cat HGG2CBBXY_7_39UDI-idt-UMI_2.fastq.gz > ND0323_R2.fastq.gz
+cat HGG2CBBXY_7_27UDI-idt-UMI_1.fastq.gz > ND0463_R1.fastq.gz
+cat HGG2CBBXY_7_27UDI-idt-UMI_2.fastq.gz > ND0463_R2.fastq.gz
+cat HGG2CBBXY_6_73UDI-idt-UMI_1.fastq.gz > ND0693_R1.fastq.gz
+cat HGG2CBBXY_6_73UDI-idt-UMI_2.fastq.gz > ND0693_R2.fastq.gz
